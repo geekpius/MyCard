e7cd7647af926b824a7a136a99949a6149157ed5
Fiifi Pius <fiifipius@gmail.com>
2023-05-10 18:22:33 +0000

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by T.K Pius on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
